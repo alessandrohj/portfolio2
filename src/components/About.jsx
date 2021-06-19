e966683d76a3883c9e090c4b150b4a1eaf20a0de
deje0014 <72152264+deje0014@users.noreply.github.com>
2021-06-19 10:38:31 -0400

    Minor css updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,9 @@ import aboutMeImg from '../imgs/undraw_about_me_wa29.svg'
 
 export default function About(){
     return (
-        <main className='min-h-screen flex place-content-center ml-2'>
-        <div className="container mx-auto font-sans pt-5">
-            <h1 className="text-2xl font-bold pb-2 pl-1">Under construction.</h1>
+        <main className='min-h-screen ml-2'>
+        <div className="container mx-auto font-sans pt-5 grid place-items-center">
+            <h1 className="text-2xl font-bold pl-1">Under construction.</h1>
             <img src={aboutMeImg} alt="About Me" className='pt-5 self-center' />
         </div>
         </main>

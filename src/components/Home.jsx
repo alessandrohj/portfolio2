6f750dd58b735f72b401c0058e3c0315bb91cf33
deje0014 <72152264+deje0014@users.noreply.github.com>
2021-06-19 10:03:19 -0400

    Updated img atl on Contact

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ export default function Home(){
     return (
        <main className='min-h-screen home-main flex place-content-center pl-1 pr-1'>
            <div className='container lg:flex-row lg:py-5 flex flex-col place-content-center items-center home pb-10'>
-           <img src={image} alt='Mobile responsive' className='responsive justify-center'/>
+           <img src={image} alt='' className='responsive justify-center'/>
            <div className='relative flex flex-col sm:responsive items-center gap-4'>
                <h1 className='text-7xl text-center text-black font-bold cursive leading-none lg:leading-snug self-center pt-5'>Hey there! <br/> I'm Alessandro.</h1>
                <h3 className='text-lg text-black text-center font-bold leading-none lg:leading-snug'>A developer trained to design and develop user-friendly websites and applications.</h3>

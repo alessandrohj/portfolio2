10069be13b8e1939ce7627c003b33e706dac301d
deje0014 <72152264+deje0014@users.noreply.github.com>
2021-06-19 10:07:51 -0400

    Removed redundant alt atribute

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ export default function Contact(){
         <div className="container mx-auto font-sans grid place-items-center">
             <h1 className="text-2xl font-bold">Under construction.</h1>
             <p className="py-3 pb-8 font-bold">In the meantime, you can find me on <a href="https://www.linkedin.com/in/j-alessandro/" target='_blank' rel='noopener noreferrer' className='text-blue-500 text-xl font-bold'>Linkedln</a> .</p>
-        <img src={contactImg} alt="Contact Image" classNam='ml-2' />
+        <img src={contactImg} alt='' classNam='ml-2' />
         </div>
         </main>
     )

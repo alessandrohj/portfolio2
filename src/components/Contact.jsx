460ae0a40136726ff1c315a6095b476886e335e6
deje0014 <72152264+deje0014@users.noreply.github.com>
2021-06-22 11:10:02 -0400

    Fixed typo

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import contactImg from '../imgs/undraw_contact_us_15o2.svg'
 export default function Contact(){
     return (
         <main className='min-h-screen pt-5'>
-        <div className="container mx-auto font-sans flex place-content-center">
+        <div className="container mx-auto font-sans grid place-items-center">
             <h1 className="text-2xl font-bold">Under construction.</h1>
             <p className="py-3 pb-8 font-bold">In the meantime, you can find me on <a href="https://www.linkedin.com/in/j-alessandro/" target='_blank' rel='noopener noreferrer' className='text-blue-500 text-xl font-bold'>Linkedln</a> .</p>
         <img src={contactImg} alt='' classNam='ml-2' />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer(){
         <footer className='bg-indigo-900 pl-6'>
             <div className='container grid grid-cols-3 mx-auto'>
                 <div className='text-white place-self-center col-span-2 text-center lg:text-start lg:justify-self-start'>
-                    <p>Copyright © Alessandro de Jesus 2021 </p>
+                    <p>© Alessandro de Jesus 2021 </p>
                 </div>
         <div className='inline-flex my-6 ml-4 row-start-1 col-start-3 justify-end'>
         <SocialIcon url='https://github.com/deje0014' className='mr-4 hover:shadow-lg social-icon'  target='_blank' fgColor='#fff' style={{height: 37, width: 37}} />

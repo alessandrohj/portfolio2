4652b89b9912b7d64ee3baa4a7ac0c40ac4b590c
Alessandro de Jesus <alessandrodejesus@Alessandros-MBP.hitronhub.home>
2022-03-05 22:58:01 -0500

    Updated About for mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,44 +2,43 @@ import React from 'react';
 import aboutMeImg from '../imgs/undraw_about_me_wa29.svg'
 
 export default function About(){
-    let spanClass = 'flex gap-4 m-2';
+    let spanClass = 'grid grid-cols-2 m-2 place-content-between text-left';
+    let titleClass ='font-bold text-left ml-10 pl-5';
     return (
       <section id='about'>
-         <h1 className='text-5xl font-bold flex justify-center cursive'>Tech Experience</h1>
-        <div className='flex'>
-        <div className="container mx-auto font-sans pt-5 grid place-items-center">
-            <img src={aboutMeImg} alt="About Me" className='pt-5 self-center h-2/3' />
+         <h1 className='text-5xl font-bold flex justify-center cursive'>About Me</h1>
+        <div className='flex flex-col sm:flex-rol mb-10'>
+        <div className="container mx-auto font-sans sm:pt-5 grid place-items-center">
+            <img src={aboutMeImg} alt="About Me" className='sm:pt-5 self-center h-2/3' />
         </div>
-        <div className="container mt-10 font-sans flex gap-3 place-items-center">
-          <div className='titles'>
+        <div className="container sm:mt-10 font-sans place-items-center text-left">
+          <div className='titles '>
             <span className={spanClass}>
-            <p className=' font-bold'>Name:</p>
-            <p className='pl-5 text-right'>Alessandro de Jesus</p>
+            <p className={titleClass}>Name:</p>
+            <p>Alessandro de Jesus</p>
             </span>
             <span className={spanClass}>
-            <p className=' font-bold'>Location:</p>
+            <p className={titleClass}>Location:</p>
             <p>Ottawa, Canada</p>
             </span>
             <span className={spanClass}>
-            <p className=' font-bold'>Degrees:</p>
+            <p className={titleClass}>Degrees:</p>
             <span>
-            <p className='pl-1'>Mobile Application Design and Development @ Algonquin College</p>
-            <p className='pl-1'>Post-Graduate Degree in Software Engineering @ PUC Minas Brazil</p>
+            <p>Mobile Application Design and Development @ Algonquin College</p>
+            <p>Post-Graduate Degree in Software Engineering @ PUC Minas Brazil</p>
             </span>
             </span>
             <span className={spanClass}>
-            <p className='font-bold'>Experience:</p>
+            <p className={titleClass}>Experience:</p>
             <p>React Developer | Web Developer | Full Stack Developer</p>
             </span>
             <span className={spanClass}>
-            <p className='font-bold'>Languages:</p>
+            <p className={titleClass}>Languages:</p>
             <p>English</p>
-            <p>Portuguese</p>
             </span>
             <span className={spanClass}>
-            <p className=' font-bold'>Contact:</p>
-            <i class="fas fa-envelope-open-text fa-2x mr-2"></i>
-            <p className='pl-1'>email@email.com</p>
+            <p className={titleClass}>Contact:</p>
+            <p>contact@alessandroj.ca</p>
             </span>
             {/* <p>Location:</p>
             <p>Degrees:</p>

ecda96e6e2f313b3798d9186dea567c5bd3c7798
Alessandro de Jesus <alessandrodejesus@Alessandros-MBP.hitronhub.home>
2022-03-05 21:47:54 -0500

    Updated cards style

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -27,52 +27,39 @@ export default function Project() {
         .catch(console.error)
     }, [])
 
-
+let buttonClass = "rounded-lg hover:border p-1 hover:bg-blue-400 hover:text-white font-semibold hover:shadow-md"
 
     return (
-<main className='min-h-screen p-10 ml-3' id="projects-section">
-            <section className='container mx-auto'>
-                <div className='flex flex-col place-content-center items-center sm:relative lg:pb-2 md:gap-10'>
-                <h1 className='text-7xl text-center cursive'>Projects</h1>
-                    <img src={CVImage} alt="CVImage" className='bg-none responsive CVImage mb-1' />
+<section id="projects-section">
+            <div className='grid grid-cols-2'>
+                <img src={CVImage} alt="CVImage" className='bg-none max-w-sm ml-7 CVImage' />
+                <h1 className='text-5xl font-bold text-left'>Projects</h1>
                 </div>
-                <div className='grid gap-10 sm:grid-cols-2 md:grid-cols-3'>
-                    {projectData && projectData.map((project, index)=> (
-                     <div className="rounded overflow-hidden shadow-xl project-card border-2 border-indigo-100" key={index}>
-                         
-                         {
-                             project.link ? <a href={project.link} target="_blank" rel="noopener noreferrer">
-                       <img className="w-full lazy project-image" src={project.mainImage.asset.url} alt={project.title} key={index} />
-                       </a> : <a href={project.codeURL} target="_blank" rel="noopener noreferrer">
-                       <img className="w-full lazy project-image" src={project.mainImage.asset.url} alt={project.title} key={index}/>
-                       </a> 
-                         }
-                       <div className="px-6 py-4">
-                         <div className="font-bold text-xl mb-2 project-title">{project.title}</div>
-                         <p className="text-gray-700 text-base">
-                           {project.description}
-                         </p>
-                       </div>
-                       <div className="px-6 pt-4 pb-2 flex flex-wrap items-center">
-                       {
+                <div className='wrapper m-5 gap-4 grid grid-cols-11 mt-2 '>
+                {projectData && projectData.map((project, index)=> (
+                    <div className=' antialiased lg:col-span-2' key={index}>
+                <img src={project.mainImage.asset.url} alt={project.title} key={index} className='w-full rounded-lg border-2 border-gray-400' />
+                <div className="description  min-h-fit border relative px-6 m-3 -mt-14 bg-white rounded-3xl shadow-md ">
+                <h1 className='font-bold uppercase leading-tight m-1'>{project.title}</h1>
+                    <p className="text-description">{project.description}</p>
+                    {
                                     project.tags.map((tag, index)=>(
                                         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 project-tag" key={index}>#{tag}</span>
                                     ))
                             }
-                     </div>
-                     <div className='project-links flex justify-center items-baseline'>
-                     {
-                                   project.link ? <a href={project.link} target="_blank" rel="noopener noreferrer" className='project-live-link bg-indigo-100'>Live Version</a> : ''
+
+                    <span className='flex gap-5 place-content-around m-3'>
+                    {
+                                   project.link ? <a href={project.link} target="_blank" rel="noopener noreferrer" className={buttonClass}>Live Version</a> : ''
                                }
                                {
-                                   project.codeURL ?  <a href={project.codeURL} target="_blank" rel="noopener noreferrer" className='project-code-link bg-indigo-100'>Code</a> : ''
+                                   project.codeURL ?  <a href={project.codeURL} target="_blank" rel="noopener noreferrer" className={buttonClass}>Code</a> : ''
                                }
-                             </div>
-                     </div>
-                    
-        ))}
+                    </span>
+                    </div>
+                    </div>
+                ))}
                 </div>
-            </section>
-        </main>
+        </section>
     )
 }
